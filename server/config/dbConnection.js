10f6b9df8704716e8b0b1337f0a01fade9ec46fe
Raish10100 <raishgma101001@gmail.com>
2024-05-29 16:07:47 +0530

    Successfully connected MongoDB database to Express server

---
+++ server/config/dbConnection.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+// Allow flexible querying  doesn't strictly match the schema
+mongoose.set('strictQuery', false); 
+
+const connectToDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(
+            process.env.MONGO_URI || 'mongodb://localhost:27017/lms'
+        );
+        if (connection) {
+            console.log(`Connected to MongoDB: ${connection.host}`);
+        }
+    } catch (e) {
+        console.error('Error: ', e);
+        process.exit(1);
+    }
+}
+ 
+export default connectToDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import app from './app.js';
 import {config} from 'dotenv'
+import connectToDB from './config/dbConnection.js';
 config(); // loads the .env file
 
 
 const PORT = process.env.PORT || 5010;  
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await connectToDB()
     console.log(`Server is running at http:localhost:${PORT}`)
 })    
