fc5647f7471bed1ec6f130053057972fc1f87c11
Raish10100 <raishgma101001@gmail.com>
2024-05-29 15:20:44 +0530

    Integrated Morgan library for enhanced logging and request monitoring

--- server/app.js
+++ server/app.js
@@ -1,25 +1,29 @@
 import express from 'express';
 import cors from 'cors'
+import morgan from 'morgan';
 
-const app = express(); 
+const app = express(); // creating instance of express
 
 
-app.use(express.json());
+app.use(express.json());// for parsing incoming JSON payloads 
 
-app.use(cors({
+app.use(cors({ 
     origin: [process.env.FRONTEND_URL],
     credentials: true
 }))
 
+app.use(morgan('dev'))
 
 app.use('/ping', function(req, res){
-    res.send('/pong')
+    res.send('pong')
 })
-
+ 
 //routes
 
+
+// if no routes are matched then send 404 page not found
 app.all('*', (req, res) => {
     res.status(404).send('OOPS! 404 page not found');
 })
 
-export default app;
+export default app; 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import app from './app.js';
+import {config} from 'dotenv'
+config(); // loads the .env file
 
-const PORT = process.env.PROCESS || 5000;  
+
+const PORT = process.env.PORT || 5010;  
+
+
 app.listen(PORT, () => {
     console.log(`Server is running at http:localhost:${PORT}`)
-})  
+})    
