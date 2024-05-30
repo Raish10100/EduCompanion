00844813d28dab182a443ad09b017ca87d82db9d
Raish10100 <raishgma101001@gmail.com>
2024-05-30 07:41:41 +0530

    Implement initial user registration controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,38 @@
-const register = (req, res, next) => {
+import User from "../models/user.model";
+
+const register = async (req, res, next) => {
     const { fullName, email, password } = req.body;
 
     if(!fullName || !email || !password) {
         return next(new AppError("All fields are required", 400));
+    };
+
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        return next(new AppError('Email already exists', 400));
     }
+
+    const user = await User.create({
+        fullName,
+        email,
+        password,
+        avatar: {
+            public_id: "random",      // temporary   
+            secure_url: "random"     // temporary
+        }
+    })
+
+    if (!user) {
+        return next(new AppError('User registration failed,Please try again later', 400))
+    }
+
+    //TODO: File upoad to cloudinary and save the url in user model.
+
+
+    await user.save();
+
+
 };
 
 const login = (req, res) => {
