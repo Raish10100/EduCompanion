5bc8b2f1e370928175df8485abfa8b153ab6c434
Raish10100 <raishgma101001@gmail.com>
2024-06-01 15:11:26 +0530

    Fix bug found and resolved during testing of API endpoint (api/v1/user/login)

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -94,7 +94,7 @@ const login = async (req, res, next) => {
             email 
         }).select('+password');
     
-        if (!user || !user.comparePassword(password)) {
+        if (!(user && (await user.comparePassword(password)))) {
             return next(new AppError('Email or password is incorrect', 400));
         }
     
