a106d2234e4313361664657e0e90b9a69f99f828
Raish10100 <raishgma101001@gmail.com>
2024-06-03 07:58:11 +0530

    Implemented course creation functionality for ADMIN

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,4 +1,7 @@
 import Course from "../models/course.model.js"
+import AppError from "../utils/error.util.js";
+import cloudinary from 'cloudinary';
+import fs from 'fs/promises'
 
 const getAllCourses = async (req, res, next) => {
     try {
@@ -37,8 +40,65 @@ const getLecturesByCourseId = async (req, res, next) => {
     }
 }
 
+const createCourse = async(req, res, next) => {
+    const { title, description, category, createdBy } = req.body;
+// console.log(title)
+    if(!title || !description || !category || !createdBy) {
+        return next(
+            new AppError("All fields are required", 400)
+        )
+    }
+
+    const course = await Course.create({
+        title,
+        description,
+        category,
+        createdBy,
+        thumbnail: {
+            public_id:"Dummy",
+            secure_url: "Dummy"
+        }
+    })
+
+    if(!course) {
+        return next(
+            new AppError('Unable to create course', 500)
+        )
+    }
+
+    try {
+            if(req.file) {
+                const result = await cloudinary.v2.uploader.upload(req.file.path, {
+                    folder: 'lms'
+                });
+                // console.log('debugg')
+    // console.log(result.public_id);
+    // console.log(result.secure_url);
+                if(result) {
+                    course.thumbnail.public_id = result.public_id;
+                    course.thumbnail.secure_url = result.secure_url;
+                }
+        
+                fs.rm(`uploads/${req.file.filename}`);
+            }
+        
+            await course.save();
+        
+            res.status(200).json({
+                success: true,
+                message: 'Successfully created course',
+            })
+        
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })  
+    }
+}
 
 export {
     getAllCourses,
-    getLecturesByCourseId
+    getLecturesByCourseId,
+    createCourse
 }
--- server/routes/course.router.js
+++ server/routes/course.router.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
-import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controller.js';
+import { createCourse, getAllCourses, getLecturesByCourseId } from '../controllers/course.controller.js';
 import isLoggedIn from '../middlewares/auth.middleware.js';
+import upload from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
 
-router.get('/', getAllCourses);
+router.route('/')
+                .get(getAllCourses)
+                .post(
+                    upload.single('thumbnail'),
+                    createCourse
+                )
 
 router.get('/:id',isLoggedIn, getLecturesByCourseId)
 
