4511658034d23fcec755218706f5a21f82bac04f
Raish10100 <raishgma101001@gmail.com>
2024-06-10 08:04:08 +0530

    Added labels to fields on authentication pages

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -60,31 +60,36 @@ function Login(){
         <form
           onSubmit={onLogin}
         //   noValidate
-          className="dark:bg-[#c6cedd16] bg-[#c0c3c9ef] shadow-2xl w-[90%] sm:w-[60%] lg:w-[40%]  flex-col flex justify-center items-center py-6 gap-10 rounded "
+          className="dark:bg-[#c6cedd16] bg-[#c0c3c9ef] shadow-2xl w-[90%] sm:w-[60%] lg:w-[40%]  flex-col flex justify-center items-center py-3 gap-6 rounded"
         >
           <h1 className="text-xl vs:text-3xl text-black dark:text-white font-bold">LogIn</h1>
-          <div className="fields w-[90%] flex flex-col justify-center items-center gap-6">
-            
-            <input
-              required
-              type="email"
-              placeholder="Email Address"
-              name="email"
-              id="email"
-              className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
-              value={loginData.email}
-              onChange={handleUserInput}
-            />
-            <input
-              required
-              type="password"
-              placeholder="Password"
-              name="password"
-              id='password'
-              className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
-              value={loginData.password}
-              onChange={handleUserInput}
-            />
+          <div className="fields w-[90%] flex flex-col justify-center items-center gap-3">
+            <div className="w-[100%] flex flex-col gap-1">
+              <label htmlFor="email" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Email</label>
+              <input
+                required
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                id="email"
+                className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
+                value={loginData.email}
+                onChange={handleUserInput}
+              />
+            </div>
+            <div className="w-[100%] flex flex-col gap-1">
+              <label htmlFor="password" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Password</label>
+              <input
+                required
+                type="password"
+                placeholder="Password"
+                name="password"
+                id='password'
+                className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
+                value={loginData.password}
+                onChange={handleUserInput}
+              />
+            </div >
             <button  type="submit" className="signup-btn dark:bg-[#ffffffe5] bg-[#000000] rounded text-white active:bg-[#000000a2] dark:active:bg-[#ffffff7f] transition-all ease-in-out duration-300 border-none px-3 py-3 w-[100%] dark:text-black font-semibold text-md sm:text-xl">
               LogIn
             </button>
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -102,54 +102,66 @@ function SignUp() {
         <form
           onSubmit={createNewAccount}
         //   noValidate
-          className="dark:bg-[#c6cedd16] bg-[#c0c3c9ef] shadow-2xl w-[90%] sm:w-[60%] lg:w-[40%]  flex-col flex justify-center items-center py-6 gap-10 rounded "
+          className="dark:bg-[#c6cedd16] bg-[#c0c3c9ef] shadow-2xl w-[90%] sm:w-[60%] lg:w-[40%]  flex-col flex justify-center items-center py-3 gap-6 rounded "
         >
           <h1 className="text-xl vs:text-3xl text-black dark:text-white font-bold">Create Account</h1>
-          <div className="fields w-[90%] flex flex-col justify-center items-center gap-6">
-            <input
-              required
-              type="text"
-              placeholder="Username"
-              name="fullName"
-              id="fullName"
-              className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
-              value={signupData.fullName}
-              onChange={handleUserInput}
-            />
-            <input
-              required
-              type="email"
-              placeholder="Email Address"
-              name="email"
-              id="email"
-              className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
-              value={signupData.email}
-              onChange={handleUserInput}
-            />
-            <input
-              required
-              type="password"
-              placeholder="Password"
-              name="password"
-              id='password'
-              className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
-              value={signupData.password}
-              onChange={handleUserInput}
-            />
-            <div className=" vs:w-[100%] w-[100%]  text-wrap custom-group-avatar">
+          <div className="fields w-[90%] flex flex-col justify-center items-center gap-4">
+            <div className="w-[100%] flex flex-col gap-1">
+            <label htmlFor="Name" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Name</label>
               <input
-              onChange={getImage}
-                type="file"
-                className=" absolute  opacity-0 bg-black  overflow-x-hidden max-w-[100px] h-[40px] vs:max-w-[1000px] mt-4 vs:mt-3 ml-2 vs:px-8 sm:px-24 cursor-pointer"
-                id="image_uploads"
-                accept=".jpg, .jpeg, .png"
+                required
+                type="text"
+                placeholder="Enter your name"
+                name="fullName"
+                id="fullName"
+                className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
+                value={signupData.fullName}
+                onChange={handleUserInput}
               />
-              <button className=" flex justify-between flex-wrap px-3 py-3 w-[100%]   tracking-widest border-black   dark:border-[#ffffff91] border-2 text-md sm:text-xl rounded bg-transparent cursor-pointer">
-                <span className="avatar-btn-hover bg-black  text-white text-wrap  dark:bg-[#ffffffd0] transition-all ease-in-out duration-300   dark:text-black tracking-normal px-2 py-1 cursor-pointer rounded font-semibold">
-                { fileName ? `Avatar:  ${fileName}` : "Choose your avatar "}
-                </span>
-                <span className="dark:text-[#5f6cf6] text-[#e45f5f] font-bold">{fileName ? "" :"(Optional)"}</span>
-              </button>
+            </div>
+            <div className="w-[100%] flex flex-col gap-1">
+              <label htmlFor="Email" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Email</label>
+              <input
+                required
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                id="email"
+                className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
+                value={signupData.email}
+                onChange={handleUserInput}
+              />
+            </div>
+            <div className="w-[100%] flex flex-col gap-1">
+              <label htmlFor="Password" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Password</label>
+              <input
+                required
+                type="password"
+                placeholder="Enter your password"
+                name="password"
+                id='password'
+                className="px-3 py-3 w-[100%] outline-none dark:hover:border-[#fff] border-black transition-all ease-in-out duration-300 tracking-widest text-black  dark:text-white dark:border-[#ffffff91] text-md sm:text-xl  border-2 rounded bg-transparent placeholder:text-black placeholder:dark:text-white"
+                value={signupData.password}
+                onChange={handleUserInput}
+              />
+            </div>
+            <div className="w-[100%] flex flex-col gap-1">
+              <label htmlFor="Avatar" className="text-start w-[100%] text-xl font-bold dark:text-white text-black">Avatar</label>
+              <div className=" vs:w-[100%] w-[100%]  text-wrap custom-group-avatar">
+                <input
+                onChange={getImage}
+                  type="file"
+                  className=" absolute  opacity-0 bg-black  overflow-x-hidden max-w-[100px] h-[40px] vs:max-w-[1000px] mt-4 vs:mt-3 ml-2 vs:px-8 sm:px-24 cursor-pointer"
+                  id="image_uploads"
+                  accept=".jpg, .jpeg, .png"
+                />
+                <button className=" flex justify-between flex-wrap px-3 py-3 w-[100%]   tracking-widest border-black   dark:border-[#ffffff91] border-2 text-md sm:text-xl rounded bg-transparent cursor-pointer">
+                  <span className="avatar-btn-hover bg-black  text-white text-wrap  dark:bg-[#ffffffd0] transition-all ease-in-out duration-300   dark:text-black tracking-normal px-2 py-1 cursor-pointer rounded font-semibold">
+                  { fileName ? `Avatar:  ${fileName}` : "Upload your avatar"}
+                  </span>
+                  <span className="dark:text-[#5f6cf6] text-[#e45f5f] font-bold">{fileName ? "" :"(Optional)"}</span>
+                </button>
+              </div>
             </div>
             <button  type="submit" className="signup-btn dark:bg-[#ffffffe5] bg-[#000000] rounded text-white active:bg-[#000000a2] dark:active:bg-[#ffffff7f] transition-all ease-in-out duration-300 border-none px-3 py-3 w-[100%] dark:text-black font-semibold text-md sm:text-xl">
               Sign Up
